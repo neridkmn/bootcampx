2948bb6d258dff64ed422ee113896d0ee18687b1
Neriman Dikmen <sanerneriman@gmail.com>
2023-08-09 16:35:03 -0400

    Add pool.query function to place SQL commands as JavaScript

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,5 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
 
 
