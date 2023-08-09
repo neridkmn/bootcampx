0e256ace2190712bc11eaa019aa01d95686d5e49
Neriman Dikmen <sanerneriman@gmail.com>
2023-08-09 16:41:55 -0400

    Add more JavaScript after res.rows

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,8 +13,8 @@ FROM students
 LIMIT 5;
 `)
 .then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
-
+  res.rows.forEach(user => { // res.rows return a JS object so we can add more JS code after that. 
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+  })
+});
 
