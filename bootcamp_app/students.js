ef633c9743994aa3a258c480a53ea101df1bf5fe
Neriman Dikmen <sanerneriman@gmail.com>
2023-08-09 16:55:13 -0400

    Insert the process.argv directly into the query by using the template strings.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,11 +11,12 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
-  res.rows.forEach(user => { // res.rows return an array of JS object so we can add more JS code after that. 
+  res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
 
